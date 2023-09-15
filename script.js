081dd133491e432446c6c84a6d87eb3a5735959a
Mnemos29 <jouer-ecosse0n@icloud.com>
2023-09-15 21:29:50 +0200

    add script balise with importmap for cdn

--- script.js
+++ script.js
@@ -123,9 +123,9 @@ function changePlayer(numPlayer) {
 
 //=======================
 //dice with threejs
-//import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 
 const scene = new THREE.Scene()
